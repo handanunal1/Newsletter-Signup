6325de015e0213c1a86f544babd0042c60e0b659
Handan <handann.unal@gmail.com>
2020-09-09 22:19:48 +0300

    DiceGame

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,3 +1,8 @@
-var randomNumber1 = Math.floor(Math.random() * 7);
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
 
-document.querySelector("img").setAttribute("src","images/dice"+randomNumber1+".png");
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+image1.setAttribute("src","images/dice"+randomNumber1+".png");
+image2.setAttribute("src","images/dice"+randomNumber2+".png");
